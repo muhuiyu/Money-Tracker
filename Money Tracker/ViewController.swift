0e001a1eefcaeaafcccb81b911f55320a480343b
mlsg-gracemy <grace.yu@monstar-lab.com>
2023-07-30 15:31:52 +0800

    Initial Commit

---
+++ Money Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Tracker
+//
+//  Created by Grace, Mu-Hui Yu on 7/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
